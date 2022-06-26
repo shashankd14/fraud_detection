5a4602d2e36ece009266fedc2b2ca8d90805e1f6
shashankd14 <shashank.rd@gmail.com>
2022-06-27 00:10:50 +0530

    model 2 is final in production

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 print("adding all ml code here")
-git
+
+
+def model1():
+    print("adding code to model 1")
+
+def model2():
+    print("model 2 is the best performing")
+
+def model3():
+    print("adding code to model 3")
