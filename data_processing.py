6d235456944f0a168af58ddc183b1083c94ba481
shashankd14 <shashank.rd@gmail.com>
2022-06-26 23:47:26 +0530

    added new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,14 @@
-print("adding all fraud detection related code")
+
+print("adding all the fraud detection related code")
+
+a=5
+b=7
+
+def sum(a,b):
+    c = a+b
+    return a
+
+
+print("added a new function sum return not correct")
+
+
